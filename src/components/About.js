ba0d1b9c59cbb98e4ddaf79469cdfe57944c13b0
Abel Koech <koechabel161@gmail.com>
2024-07-30 19:02:41 +0300

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+    <h2>About Me</h2>
+    <p>I have proficient in HTML ,CSS and Javascript also adept with react Library</p>
+    <img src={image} alt="I made this"/>
+  </div>);
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
+const h1Style={
+  color:'firebrick',
+  margin:'50px',
+};
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return( <div>
+  <h1 style={h1Style} >{name} is a Web Developer from {city}</h1>
+  </div>)
 }
 
 export default Home;
